5de9fb770c416942247570306feb89538fab155d
akashbaj03 <44369474+akashbaj03@users.noreply.github.com>
2019-09-10 21:10:13 +0100

    Update __init__.py

--- categoricaloutlier/__init__.py
+++ categoricaloutlier/__init__.py
@@ -1,3 +1,3 @@
 # Inside of __init__.py
-from categoricaloutlier.categoricaloutlier import TrainOutlier
-from categoricaloutlier.categoricaloutlier import PredictScore
+from categoricaloutlier.AnomalyTrainer import TrainOutlier
+from categoricaloutlier.AnomalyScorer import PredictScore
