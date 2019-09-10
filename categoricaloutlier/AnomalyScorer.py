84950556c45024e61fb4c702bdca3ac4df69e996
akashbaj03 <44369474+akashbaj03@users.noreply.github.com>
2019-09-10 20:56:17 +0100

    Update AnomalyScorer.py

--- categoricaloutlier/AnomalyScorer.py
+++ categoricaloutlier/AnomalyScorer.py
@@ -5,40 +5,44 @@ class PredictOutlier:
     
     def predict_anomaly_detector(self):
         cols_freq = []
-        
-        for c in anomalydetectorobject.cols:
+        if(self.anomalydetectorobject.datetimecols != None):
+            self.get_datetimefeatures()
             
-            d = anomalydetectorobject.valuecountsdict[c]
-            test_data = pd.merge(test_data,d,how='left',on=[c])
-            new_prob_value = (1/1)/(1 + anomalydetectorobject.colsum.loc[df_sum_values['index'] == c+'_frequency','sum'].values[0])
+        for c in self.anomalydetectorobject.cols:
             
-            test_data[c+'_frequency'] = test_data[c+'_frequency'].fillna(new_prob_value)
+            d = self.anomalydetectorobject.valuecountsdict[c]
+            
+            self.testdata = pd.merge(self.testdata,d,how='left',on=[c])
+            new_prob_value = (1/1)/(1 + self.anomalydetectorobject.colsum.loc[self.anomalydetectorobject.colsum['index'] == c+'_frequency','sum'].values[0])
+            
+            self.testdata[c+'_frequency'] = self.testdata[c+'_frequency'].fillna(new_prob_value)
             cols_freq.append(c+'_frequency')
 
-        test_distance = test_data[cols_freq].apply(lambda x: (mahalanobis(df_median_values['median'].values, x, anomalydetectorobject.invcovmx)), axis=1)
+        test_distance = self.testdata[cols_freq].apply(lambda x: (mahalanobis(self.anomalydetectorobject.median['median'].values, x, self.anomalydetectorobject.invcovmx)), axis=1)
 
-        self.score = sigmoid_curve(test_distance,anomalydetectorobject.threshold)
+        self.scores = self.sigmoid_curve(test_distance,self.anomalydetectorobject.threshold)
 
         return self
     
-    def sigmoid_curve(test_dist,threshold, k = 1):
+    def sigmoid_curve(self,test_dist, k = 1):
         score = []
         for t in test_dist:
-            v = 1.0/(1.0 + np.exp(-k*(np.log10(t) - np.log10(threshold))))
+            v = 1.0/(1.0 + np.exp(-k*(np.log10(t) - np.log10(self.anomalydetectorobject.threshold))))
             score.append(v)
         return score
     
-    def get_datetimefeatures(self, df):
-        for d in self.datetimecols:
+    def get_datetimefeatures(self):
+        for d in self.anomalydetectorobject.datetimecols:
             
-            df[d+'_weekday'] = self.data[d].apply(lambda m : m.weekday())
-            df[d+'_hourofday'] = self.data[d].apply(lambda m : m.hour)
-            self.cols = self.cols + [d+'_weekday',d+'_hourofday']
-        return df
+            self.testdata[d+'_weekday'] = self.testdata[d].apply(lambda m : m.weekday())
+            self.testdata[d+'_hourofday'] = self.testdata[d].apply(lambda m : m.hour)
+            #self.cols = self.cols + [d+'_weekday',d+'_hourofday']
+        
     
     def __init__(self,ad,td):
-        if(test_data.shape[0]) == 0
+        if(test_data.shape[0]) == 0:
             raise ValueError('There should be at least one observation to predict score')
         self.anomalydetectorobject = ad
         self.testdata = td
+        self.predict_anomaly_detector()
         
